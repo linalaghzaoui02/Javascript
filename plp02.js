ef0671c7c890d1643a6b651365f42cf52f199d33
linalaghzaoui02 <136354267+linalaghzaoui02@users.noreply.github.com>
2023-09-25 00:57:45 -0400

    Create plp02.js

---
+++ plp02.js
@@ -0,0 +1,26 @@
+// Integer (Note: JavaScript does not differentiate between integer and float)
+let integerVariable = 42;
+console.log("Integer:", integerVariable);
+
+// String
+let stringVariable = "Hello, World";
+console.log("String:", stringVariable);
+
+// Floating-point number
+let floatVariable = 42.42;
+console.log("Float:", floatVariable);
+
+// Boolean
+let booleanVariable = true;
+console.log("Boolean:", booleanVariable);
+
+// Array
+let arrayVariable = [1, 2, 3];
+console.log("Array:", arrayVariable);
+
+// Object (Equivalent to dictionary, hash, map)
+let objectVariable = {
+  key1: "value1",
+  key2: "value2"
+};
+console.log("Object:", objectVariable);
